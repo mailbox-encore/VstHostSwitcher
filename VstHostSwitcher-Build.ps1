0bfa197bc79872237eb4ff28dfd6cf0b04144972
encore <mailbox.encore@gmail.com>
2019-08-16 09:51:13 +0200

    added comments in build script

--- VstHostSwitcher-Build.ps1
+++ VstHostSwitcher-Build.ps1
@@ -1,3 +1,7 @@
-
-E:\Hosts\Scripts\_Tools\PS2EXE-v0.6.0.1\ps2exe.ps1 VstHostSwitcher.ps1 VstHostSwitcher.exe -verbose -noConsole -iconfile VstHostSwitcher.ico
-$NULL = Read-Host "Press enter to exit"
+###################################################################################
+# Script to build the VstHostSwitcher.exe file.
+# IMPORTANT: ps2exe.ps1 script has to be available from your PATH.
+# https://gallery.technet.microsoft.com/scriptcenter/PS2EXE-GUI-Convert-9b4b0493
+###################################################################################
+ps2exe.ps1 VstHostSwitcher.ps1 VstHostSwitcher.exe -verbose -noConsole -iconfile VstHostSwitcher.ico
+#$NULL = Read-Host "Press enter to exit"
