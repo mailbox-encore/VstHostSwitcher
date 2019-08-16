b96550d162a60302f532505d3d8c9a1a42eea014
encore <mailbox.encore@gmail.com>
2019-08-16 09:34:31 +0200

    Initial commit

---
+++ VstHostSwitcher-Build.ps1
@@ -0,0 +1,3 @@
+
+E:\Hosts\Scripts\_Tools\PS2EXE-v0.6.0.1\ps2exe.ps1 VstHostSwitcher.ps1 VstHostSwitcher.exe -verbose -noConsole -iconfile VstHostSwitcher.ico
+$NULL = Read-Host "Press enter to exit"
---
+++ VstHostSwitcher.ps1
@@ -0,0 +1,13 @@
+﻿param(
+[string]$arguments
+)
+
+
+#Write-Host $arguments
+if($arguments.StartsWith("E:\Vstplugins\")) {
+  #Write-Host x86
+  &'E:\Hosts\Tone2 - NanoHost\NanoHost32bit.exe' $arguments
+}else {
+  #Write-Host x64
+  &'E:\Hosts\Tone2 - NanoHost\NanoHost64bit.exe' $arguments
+}
