2eda742ec307103d6f4413ec8d2a109cb29fe4fa
encore <mailbox.encore@gmail.com>
2019-08-19 17:29:37 +0200

    removed extra script for functions

--- README.md
+++ README.md
@@ -10,21 +10,72 @@ If you you have both x86 and x64 version of VST PLugins and you do not always ha
 > - [Image-Line MiniHostModular](https://forum.image-line.com/viewtopic.php?f=1919&t=123031)
 > - [Hermannseib vsthost](http://www.hermannseib.com/english/vsthost.htm)
 
-## Scripts file structure
+The main idea is to create an executable that will be associated with ``.dll`` files in the MS Windows file explorer so I could double click any vst.dll file and it will be open quickly in my preferred VST host indenpendently to the fact that the .dll is a 32 or 64 bit one.
+In order to do this I need:
 
-## Customization
+- A script which contains the logic to choose between teh 32 or 64 bits VST host.
+- A tool to compile this script to a MS Widnows executable file.
+- Associate this executable with any .dll file.
+
+## Why so many .ps1  script files
+
+The ``VstHostSwitcher.ps1`` script is containing the logic to _guess_ if the ``path-to-the-vst-dl-to-launch`` is a 32 or 64 bits directory.
+The second ``VstHostSwitcher-Build.ps1`` script is used as an example file to build the ``VstHostSwitcher.ps1`` file to an executable. 
+Please see the ``Building an executable with the scripts`` section at the end of this file for more details.
+
+<span style="color:red">**IMPORTANT**</span>
+Please edit the ``VstHostSwitcher.ps1`` script at line 10 to set your own executable path location. 
+This is the place where you will store the ``VstHostSwitcher.exe`` executable and its associated ``VstHostSwitcher.ini`` file as otherwise the ``VstHostSwitcher.exe`` executable file won't be able to found the ``VstHostSwitcher.ini``. The current script location cannot be used at runtime as it becomes the VST plugin ``.dll`` file location unfortunately... 
+I could not found a way to fix this problem therefore any sugestions will be greatly appreciated. Thanks in advance for any help fixing this problem.  
+
+![Important-Warning-Image](images/Important-Warning.png)
+
+## Paths customization
 
 As there is not really any standardisation for the VST plugins and host location it is possible to define your own directory structure to store your VST plugins and host locations.
-These settings are therefore stored in the ``VstHostSwitcher.ini`` file which must be stored oin the same directory as the previously described scripts.
+These settings are therefore stored in the ``VstHostSwitcher.ini`` file which must be stored in the same directory as the previously described scripts.
 This ``VstHostSwitcher.ini`` file has the following structure:
 
-Definitions for the x86 and x64 VstPlugin folder paths:
+**Definitions for the x86 and x64 *VstPlugin* folder paths:**
 
 > \[VstPluginFolderPaths\]  
 > x86=\[enter your x86 VST plugins path here\]  
 > x64=\[enter your x64 VST plugins path here\]  
 
-Notice that if no path is defined, the script will try to get it from the MS Widnwos registry or use the following default values rspectively for the x86 and x64 default VST plugin path location:
+Notice that if no VST plugins folder path is defined, the script will try to get it from the MS Widows registry or use the following default values respectively for the x86 and x64 default path location:
 
 > - C:\Program Files (x86)\Steinberg\VstPlugins\
 > - C:\Program Files\Steinberg\VstPlugins\
+
+**Definitions for the x86 and x64 VST _host(s)_ folder paths:**
+
+> \[VstHostApplicationsPaths\]  
+> preferred=Path2
+> x86Path1=\[enter your first x86 VST HOST path here\]  
+> x64Path1=\[enter your first x64 VST HOST path here\]
+> x86Path2=\[enter your second x86 VST HOST path here\]
+> x64Path2=\[enter your second x64 VST HOST path here\]
+>...
+
+You can enter several pairs (x86 + x64) of VST host paths and specify your preferred one using the optionnal _preferred_ key. If the _preferred_ is not specified the script will use the __xnnPath1__ pair of paths as default.
+
+Notice that if no VST HOST application path is defined, the script will use the following default values respectively for the x86 and x64 default path location which will work obviously only if these path are existing on your machine... which I doubt, so I strongly advise you fill the ``VstHostSwitcher.ini`` with existing path extries...;o)
+
+> E:\Hosts\Tone2 - NanoHost\NanoHost32bit.exe
+> E:\Hosts\Tone2 - NanoHost\NanoHost64bit.exe
+
+## Building an executable with the scripts
+
+Now that you have updated the ``VstHostSwitcher.ini`` file with your own setings you can try to build the executable. To do so you will need the great [ps2exe script that you can download here](https://gallery.technet.microsoft.com/scriptcenter/PS2EXE-GUI-Convert-9b4b0493)
+
+Once you have downloaded and extracted the zip file (i.e. for me in the _E:\Dev\Tools\PS2EXE_ directory) I advise you to add the folder location where you unzip the file to your PATH environment variable. This way, you can simply run the ``VstHostSwitcher-Build.ps1`` script.
+Here is how to do so in MS Windows 10 file explorer:
+
+![SetEnv-Image](images/SetEnv.png)
+
+Otherwise, please simply prefix the ps2exe.ps1 string with the path to the ``ps2exe.ps1`` script location in the ``VstHostSwitcher-Build.ps1`` script like this:
+``E:\Dev\Tools\PS2EXE\ps2exe.ps1 VstHostSwitcher.ps1 VstHostSwitcher.exe -verbose -noConsole -iconfile VstHostSwitcher.ico``
+
+Once you run the ``VstHostSwitcher-Build.ps1`` it will create then the  ``VstHostSwitcher.exe`` file.
+Notice that you can also specify your own icon file name if neded.
+I found [the one in this example here](https://icon-icons.com/)...
--- VstHostSwitcher.ps1
+++ VstHostSwitcher.ps1
@@ -1,30 +1,110 @@
 ﻿param(
-    [string]$arguments
+  [string]$arguments
 )
-# 'import' file containing utility functions
-. .\VstHostSwitcher-Functions.ps1
 
-Add-Type -AssemblyName PresentationCore,PresentationFramework
+#############################################################################################
+# IMPORTANT: the executable deployment location must be hardcode in the variable below BEFORE
+# building the executable  because, at runtime, the current location will be the one of the
+# VST .dll file and therfore the .ini file could not be found.
+#############################################################################################
+$applicationExePath="Enter the full path to VstSwitcher.exe here please"
 
-
-$VerbosePreference = "Continue" #"SilentlyContinue"
-# for debug only
-# $Debug=$true
+# Uncomment the following line to debug 
+#$Debug=$true
 if ($Debug -and !$arguments) {
+  $VerbosePreference = "Continue" 
   #$arguments = "E:\VstPlugins\U-HE\ACE.dll"
   #$arguments="E:\VstPlugins.x64\U-HE\ACE(x64).dll"
  
-  $arguments = "E:\Vstplugins\VK-1\VK-1 Viking Synthesizer.dll"
+ #$arguments = "E:\Vstplugins\VK-1\VK-1 Viking Synthesizer.dll"
   #$arguments = "E:\Vstplugins.x64\Boost11\Boost11_64.dll"
   #$arguments = "E:\VstPlugins\U-HE\ACE.dlna"
+  $applicationExePath="E:\Hosts\VstHostSwitcher"
+  if (!(Test-Path $applicationExePath)) {
+    # USE current location during development
+    $applicationExePath=Get-Location
+  }
+  Write-Verbose "applicationExePath=[$applicationExePath]"
+  Write-Verbose "arguments=[$arguments]"
 }
-Write-Verbose "arguments=$arguments"
+
+if (!$arguments -or !$arguments.EndsWith(".dll")) {
+  Add-Type -AssemblyName PresentationCore,PresentationFramework
+  $ButtonType = [System.Windows.MessageBoxButton]::OK
+  $MessageIcon = [System.Windows.MessageBoxImage]::Error
+  $MessageBody = "VstHostSwitcher needs a VST .dll file path as argument."
+  $MessageTitle = "Error  message"
+  $result=[System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
+  Write-Verbose  "result=[$result]"
+  Exit
+}
+
+# Utility functions to parse .ini  files
+  # https://gallery.technet.microsoft.com/scriptcenter/ea40c1ef-c856-434b-b8fb-ebd7a76e8d91
+  Function Get-IniContent {  
+    [CmdletBinding()]  
+    Param(  
+        [ValidateNotNullOrEmpty()]  
+        [ValidateScript({(Test-Path $_) -and ((Get-Item $_).Extension -eq ".ini")})]  
+        [Parameter(ValueFromPipeline=$True,Mandatory=$True)]  
+        [string]$FilePath  
+    )  
+      
+    Begin  
+        {Write-Verbose "$($MyInvocation.MyCommand.Name):: Function started"}  
+          
+    Process  
+    {  
+        Write-Verbose "$($MyInvocation.MyCommand.Name):: Processing file: $Filepath"  
+              
+        $ini = @{}  
+        switch -regex -file $FilePath  
+        {  
+            "^\[(.+)\]$" # Section  
+            {  
+                $section = $matches[1]  
+                $ini[$section] = @{}  
+                $CommentCount = 0  
+            }  
+            "^(;.*)$" # Comment  
+            {  
+                if (!($section))  
+                {  
+                    $section = "No-Section"  
+                    $ini[$section] = @{}  
+                }  
+                $value = $matches[1]  
+                $CommentCount = $CommentCount + 1  
+                $name = "Comment" + $CommentCount  
+                $ini[$section][$name] = $value  
+            }   
+            "(.+?)\s*=\s*(.*)" # Key  
+            {  
+                if (!($section))  
+                {  
+                    $section = "No-Section"  
+                    $ini[$section] = @{}  
+                }  
+                $name,$value = $matches[1..2]  
+                $ini[$section][$name] = $value  
+            }  
+        }  
+        Write-Verbose "$($MyInvocation.MyCommand.Name):: Finished Processing file: $FilePath"  
+        Return $ini  
+    }  
+          
+    End  
+        {Write-Verbose "$($MyInvocation.MyCommand.Name):: Function ended"}  
+}
+
+
 
 # Variables initialisations....
 # 1) Try first to check if there is an ini file with custom entries...
-if (Test-Path .\VstHostSwitcher.ini) {
+$iniFilePath="$applicationExePath\VstHostSwitcher.ini"
+if (Test-Path $iniFilePath) {
   # yes ! use custom definitions
-  $vstHostSwitcherIniFile = Get-IniContent .\VstHostSwitcher.ini
+  $vstHostSwitcherIniFile = Get-IniContent  $iniFilePath
   $vstPluginsPath_x86= $vstHostSwitcherIniFile.VstPluginFolderPaths.x86
   $vstPluginsPath_x64= $vstHostSwitcherIniFile.VstPluginFolderPaths.x64
   # TODO handle the preferred path here
@@ -34,17 +114,19 @@ if (Test-Path .\VstHostSwitcher.ini) {
   }
   $vstHostApplicationsPaths_x86 = $vstHostSwitcherIniFile.VstHostApplicationsPaths["x86$preferred"]
   $vstHostApplicationsPaths_x64 = $vstHostSwitcherIniFile.VstHostApplicationsPaths["x64$preferred"]
+}else{
+  Write-Error  "Cannot found: $iniFilePath"
 }
 if (!$vstHostApplicationsPaths_x86) {
   # No... Use default values if the .ini file is not existing this works ony for me ;o)
-  $vstHostApplicationsPaths_x86 ="E:\Hosts\Tone2 - NanoHost\NanoHovt32bit.exe"
+  $vstHostApplicationsPaths_x86 ="E:\Hosts\Tone2 - NanoHost\NanoHost32bit.exe"
 }
 if (!$vstHostApplicationsPaths_x64) {
   # No... Use default values if the .ini file is not existing this works ony for me ;o)
   $vstHostApplicationsPaths_x64 ="E:\Hosts\Tone2 - NanoHost\NanoHost64bit.exe"
 }
 Write-Verbose "vstHostApplicationsPaths_x86=$vstHostApplicationsPaths_x86"
-Write-Verbose "VstHostApvlicationsPaths_x64=$vstHostApplicationsPaths_x64"
+Write-Verbose "vstHostApplicationsPaths_x64=$vstHostApplicationsPaths_x64"
 
 # 2) Try to retrieve the x86 VstPlugins path from the registry 
 # if it was not defined in the .ini file
@@ -55,7 +137,7 @@ if (!$vstPluginsPath_x86) {
 if (!$vstPluginsPath_x86) {
   $vstPluginsPath_x86 = "C:\Program Files (x86)\Steinberg\VstPlugins\"
 }
-Write-Verbose "VSTPluginsPath_x86=$vstPluginsPath_x86"
+Write-Verbose "vstPluginsPath_x86=$vstPluginsPath_x86"
 
 # try to retrieve the x64 VST plugins path from the registry 
 # if it was not defined in the .ini file
@@ -69,24 +151,11 @@ if (!$vstPluginsPath_x64) {
 Write-Verbose "vstPluginsPath_x64=$vstPluginsPath_x64"
 
 # 3) Do the VST Host application switching work here...
-if ($arguments -and $arguments.EndsWith(".dll")) {
-  if ($arguments.StartsWith($vstPluginsPath_x86)) {
-    Write-Verbose "Launching x86 VST host using [$vstHostApplicationsPaths_x86] application."
-    & $vstHostApplicationsPaths_x86 $arguments
-  }
-  else {
-    Write-Verbose "Launching v64 VST host using [$vstHostApplicationsPaths_x64] application."
-    & $vstHostApplicationsPaths_x64 $arguments
-  }
-}else{
-  $ButtonType = [System.Windows.MessageBoxButton]::OK
-  $MessageIcon = [System.Windows.MessageBoxImage]::Error
-  $MessageBody = "The VstHostSwitcher script need a VST dll file path as argument."
-  $MessageTitle = "Error  message"
-  $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
-  Write-Error "Result=[$Result]"
+if ($arguments.StartsWith($vstPluginsPath_x86)) {
+  Write-Verbose "Launching x86 VST host using [$vstHostApplicationsPaths_x86] application."
+  & $vstHostApplicationsPaths_x86 $arguments
 }
-
-
-
-
+else {
+  Write-Verbose "Launching v64 VST host using [$vstHostApplicationsPaths_x64] application."
+  & $vstHostApplicationsPaths_x64 $arguments
+}
